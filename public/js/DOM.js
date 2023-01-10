6dbfd9867f2db850d168baf6cb3a0d3a487aa8b4
phuoc153258 <nguyendonphuochusc@gmail.com>
2023-01-10 17:08:39 +0700

    feat/load_location_in_side_bar

--- public/js/DOM.js
+++ public/js/DOM.js
@@ -41,3 +41,22 @@ function showMarkers() {
 function emptyMarkerArray() {
     markers = [];
 }
+
+function itemSideBar(value) {
+    return `<div class="col-6 p-3">
+                <div class="card" style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;">
+                    <div class="card-body">
+                        <img style="width: 100%;"
+                        src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Hue%2C_le_pont_Trang_Tien.jpg"
+                        alt="">
+                        <p class="my-text p-0 m-0" style="min-height: 40px;">Name: ${value.name}</p>
+                        <p class="my-text p-0 m-0">Lat: ${value.latLng.lat}</p>
+                        <p class="my-text p-0 m-0">Lng: ${value.latLng.lng}</p>
+                    </div>
+                </div>
+            </div>`;
+}
+
+function emptyItemSideBar() {
+    document.getElementById("list-side-bar-js").innerHTML = "";
+}
